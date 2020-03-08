529aedc220f9df3edd1c53f53bb1004bf5f334b5
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-03-09 01:17:13 +0200

    fix: fix error

--- src/index.js
+++ src/index.js
@@ -7,48 +7,43 @@ function eval() {
 
 function expressionCalculator(expr) {
   if (typeof expr === 'string') {
-    // console.log(1, expr);
-     expr = expr.replace(/\s*([\(\)\/\*\+-])\s*/g, ' $1 ');
-    // console.log(2, expr);
+    let left = expr.split('').filter((x) => x === '(');
+    let right = expr.split('').filter((x) => x === ')');
+    if (!(left.length === right.length)) throw 'ExpressionError: Brackets must be paired';
+    expr = expr.trim();
     let bracket;
-    while (bracket = expr.match(/\((\s.+\s)\)/)) {
-      // console.log(1, expr);
+    while ((bracket = expr.match(/\(\s*([\d\s\/\*\+-\.]+)\s*\)/))) {
       expr = expr.replace(bracket[0], ' ' + expressionCalculator(bracket[1]) + ' ');
-      // console.log(2, expr);
     }
+    expr = expr.replace(/\s*([\/\*\+-])\s*/g, ' $1 ');
+    expr = expr.trim();
+    expr = expr.replace(/-\s+-/g, ' + ');
+    while ((bracket = expr.match(/([-\d\.]+)\s+-\s+([\.\d]+)/))) {
+
+      expr = expr.replace(/([-\d\.]+)\s+-\s+([\.\d]+)/g, ' $1 + - $2 ');
+    }
+    expr = expr.replace(/\s+([\/\*])\s+-\s+([\.\d]+)/g, ' $1 - $2 ');
+    expr = expr.replace(/-\s+([\d\.]+)/g, '-$1 ');
 
     expr = (expr.trim()).split(/\s+/);
   }
-  // console.log(expr);
 
   while (expr.length > 1) {
     let index;
-    // console.log('start',expr);
     while ((index = expr.indexOf('/')) >= 0) {
-      // console.log('1/',expr);
-      if(+expr[index + 1] === 0) throw 'TypeError: Division by zero.';
+      if (+expr[index + 1] === 0) throw 'TypeError: Division by zero.';
       expr.splice(index - 1, 3, +expr[index - 1] / +expr[index + 1]);
-      // console.log('2/',expr);
     }
     while ((index = expr.indexOf('*')) >= 0) {
-      // console.log('1*',expr);
       expr.splice(index - 1, 3, expr[index - 1] * expr[index + 1]);
-      // console.log('2*',expr);
     }
     while ((index = expr.indexOf('-')) >= 0) {
-      // console.log('1-',expt);
       expr.splice(index - 1, 3, expr[index - 1] - expr[index + 1]);
-      // console.log('2-',expr);
-    }    
-    while ((index = expr.indexOf('+')) >= 0) {
-      // console.log('1+',expr);
-      expr.splice(index - 1, 3, +expr[index - 1] + +expr[index + 1]);
-      // console.log('2+',expr);
     }
-
+    while ((index = expr.indexOf('+')) >= 0) {
+      expr.splice(index - 1, 3, +expr[index - 1] + +expr[index + 1]);
+    }
   }
-
-
   return expr[0];
 }
 
